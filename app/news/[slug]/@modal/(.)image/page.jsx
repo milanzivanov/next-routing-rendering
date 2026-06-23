64ed618517f3bfeb2bbb6ba5af71fad8138ee1a8
milan <milanzivanov@users.noreply.github.com>
2026-06-23 15:30:53 +0200

    feat: implement modal image display and backdrop components for news detail view

---
+++ app/news/[slug]/@modal/(.)image/page.jsx
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "@/dummy-news";
+import ModalBackdrop from "@/components/modal-backdrop";
+
+export default async function InterceptedImagePage({ params }) {
+  const { slug } = await params;
+
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <ModalBackdrop />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
