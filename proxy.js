5a23f919eac18012269875eb8db514d0c675cb28
milan <milanzivanov@users.noreply.github.com>
2026-06-23 17:43:23 +0200

    Client side Data fetching

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export function proxy(request) {
-  console.log("Proxy request received:", request);
+  // console.log("Proxy request received:", request);
 
   return NextResponse.next();
 }
